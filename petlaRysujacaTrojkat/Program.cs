de0dd9637489df10d15e34f2db6367101e5b233b
piotrus.oleksy <piotrus.oleksy@gmail.com>
2018-07-01 15:34:10 +0200

    ok

--- petlaRysujacaTrojkat/Program.cs
+++ petlaRysujacaTrojkat/Program.cs
@@ -10,6 +10,20 @@ namespace petlaRysujacaTrojkat
     {
         static void Main(string[] args)
         {
+            int i, j, n;
+            Console.Write("Podaj n>1: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+                for (j = 1; j <= n - i; j++) Console.Write(" ");
+                for (j = 1; j <= 2 * i - 1; j++) Console.Write("*");
+                Console.WriteLine("");
+
+
+
+            }
+            Console.ReadKey();
         }
     }
 }
